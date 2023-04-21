8ea2d3c9e7b76feb1e3ee8a21bee0d0cd1476989
lbvienna <lenny.bronner@gmail.com>
2023-04-21 16:16:12 -0400

    updated unittests

--- tests/test_quantile.py
+++ tests/test_quantile.py
@@ -18,7 +18,7 @@ def test_basic_median_1():
     tau = 0.5
     x = np.asarray([[1], [1], [1], [2]])
     y = np.asarray([3, 8, 9, 15])
-    quantreg.fit(x, y, tau)
+    quantreg.fit(x, y, tau, fit_intercept=False)
     preds = quantreg.predict(x)
     # you'd think it would be 8 instead of 7.5, but run quantreg in R to confirm
     # has to do with missing intercept
@@ -65,7 +65,7 @@ def test_random_median(random_data_no_weights):
     tau = 0.5
     x = random_data_no_weights[["x0", "x1", "x2", "x3", "x4"]].values
     y = random_data_no_weights["y"].values
-    quantreg.fit(x, y, tau)
+    quantreg.fit(x, y, tau, fit_intercept=False)
     quantreg.predict(x)
     assert all(np.abs(quantreg.coefficients - [1.57699, 6.74906, 4.40175, 4.85346, 4.51814]) <= TOL)
 
@@ -75,7 +75,7 @@ def test_random_lower(random_data_no_weights):
     tau = 0.1
     x = random_data_no_weights[["x0", "x1", "x2", "x3", "x4"]].values
     y = random_data_no_weights["y"].values
-    quantreg.fit(x, y, tau)
+    quantreg.fit(x, y, tau, fit_intercept=False)
     quantreg.predict(x)
     assert all(np.abs(quantreg.coefficients - [0.17759, 6.99588, 4.18896, 4.83906, 3.22546]) <= TOL)
 
@@ -85,7 +85,7 @@ def test_random_upper(random_data_no_weights):
     tau = 0.9
     x = random_data_no_weights[["x0", "x1", "x2", "x3", "x4"]].values
     y = random_data_no_weights["y"].values
-    quantreg.fit(x, y, tau)
+    quantreg.fit(x, y, tau, fit_intercept=False)
     quantreg.predict(x)
     assert all(np.abs(quantreg.coefficients - [1.85617, 6.81286, 6.05586, 5.51965, 4.19864]) <= TOL)
 
@@ -112,7 +112,7 @@ def test_random_median_weights(random_data_weights):
     x = random_data_weights[["x0", "x1", "x2", "x3", "x4"]].values
     y = random_data_weights["y"].values
     weights = random_data_weights["weights"].values
-    quantreg.fit(x, y, tau, weights=weights)
+    quantreg.fit(x, y, tau, weights=weights, fit_intercept=False)
     quantreg.predict(x)
     assert all(np.abs(quantreg.coefficients - [1.59521, 2.17864, 4.68050, 3.10920, 9.63739]) <= TOL)
 
@@ -123,7 +123,7 @@ def test_random_lower_weights(random_data_weights):
     x = random_data_weights[["x0", "x1", "x2", "x3", "x4"]].values
     y = random_data_weights["y"].values
     weights = random_data_weights["weights"].values
-    quantreg.fit(x, y, tau, weights=weights)
+    quantreg.fit(x, y, tau, weights=weights, fit_intercept=False)
     quantreg.predict(x)
     assert all(np.abs(quantreg.coefficients - [0.63670, 1.27028, 4.81500, 3.08055, 8.69929]) <= TOL)
 
@@ -134,7 +134,7 @@ def test_random_upper_weights(random_data_weights):
     x = random_data_weights[["x0", "x1", "x2", "x3", "x4"]].values
     y = random_data_weights["y"].values
     weights = random_data_weights["weights"].values
-    quantreg.fit(x, y, tau, weights=weights)
+    quantreg.fit(x, y, tau, weights=weights, fit_intercept=False)
     quantreg.predict(x)
     assert all(np.abs(quantreg.coefficients - [3.47742, 2.07360, 4.51754, 4.15237, 9.58856]) <= TOL)
 
@@ -151,8 +151,8 @@ def test_changing_solver(random_data_no_weights):
 
     quantreg_scs = QuantileRegressionSolver(solver="SCS")
     quantreg_ecos = QuantileRegressionSolver(solver="ECOS")
-    quantreg_scs.fit(x, y, tau, save_problem=True)
-    quantreg_ecos.fit(x, y, tau, save_problem=True)
+    quantreg_scs.fit(x, y, tau, save_problem=True, fit_intercept=False)
+    quantreg_ecos.fit(x, y, tau, save_problem=True, fit_intercept=False)
 
     assert quantreg_scs.problem.value == pytest.approx(quantreg_ecos.problem.value, TOL)
 
@@ -165,8 +165,8 @@ def test_changing_solver_weights(random_data_weights):
 
     quantreg_scs = QuantileRegressionSolver(solver="SCS")
     quantreg_ecos = QuantileRegressionSolver(solver="ECOS")
-    quantreg_scs.fit(x, y, tau, weights=weights, save_problem=True)
-    quantreg_ecos.fit(x, y, tau, weights=weights, save_problem=True)
+    quantreg_scs.fit(x, y, tau, weights=weights, save_problem=True, fit_intercept=False)
+    quantreg_ecos.fit(x, y, tau, weights=weights, save_problem=True, fit_intercept=False)
 
     assert quantreg_scs.problem.value == pytest.approx(quantreg_ecos.problem.value, TOL)
 
@@ -183,14 +183,14 @@ def test_saving_problem(random_data_no_weights):
 
     quantreg = QuantileRegressionSolver(solver="ECOS")
 
-    quantreg.fit(x, y, tau, save_problem=False)
+    quantreg.fit(x, y, tau, save_problem=False, fit_intercept=False)
     assert quantreg.problem is None
 
-    quantreg.fit(x, y, tau, save_problem=True)
+    quantreg.fit(x, y, tau, save_problem=True, fit_intercept=False)
     assert quantreg.problem is not None
 
     # testing whether overwrite works
-    quantreg.fit(x, y, tau, save_problem=False)
+    quantreg.fit(x, y, tau, save_problem=False, fit_intercept=False)
     assert quantreg.problem is None
 
 
@@ -208,11 +208,11 @@ def test_weight_normalization_divide_by_zero(random_data_no_weights):
     quantreg = QuantileRegressionSolver(solver="ECOS")
 
     # Will succeed without weight normalization
-    quantreg.fit(x, y, tau, normalize_weights=False, weights=weights)
+    quantreg.fit(x, y, tau, normalize_weights=False, weights=weights, fit_intercept=False)
 
     # Will fail with weight normalization
     with pytest.raises(ZeroDivisionError):
-        quantreg.fit(x, y, tau, normalize_weights=True, weights=weights)
+        quantreg.fit(x, y, tau, normalize_weights=True, weights=weights, fit_intercept=False)
 
 
 def test_weight_normalization_same_fit(random_data_weights):
@@ -240,6 +240,7 @@ def test_weight_normalization_same_fit(random_data_weights):
 def test_regularization_with_intercept(random_data_no_weights):
     tau = 0.5
     x = random_data_no_weights[["x0", "x1", "x2", "x3", "x4"]].values
+    x[:,0] = 1
     y = random_data_no_weights["y"].values
 
     quantreg = QuantileRegressionSolver()
@@ -253,6 +254,16 @@ def test_regularization_with_intercept(random_data_no_weights):
     quantreg.fit(x, y, tau, save_problem=True)
     assert quantreg.problem.value < objective_w_reg
 
+def test_regularization_with_intercept_warning(random_data_no_weights, caplog):
+    caplog.clear()
+    tau = 0.5
+    x = random_data_no_weights[["x0", "x1", "x2", "x3", "x4"]].values
+    y = random_data_no_weights["y"].values
+
+    quantreg = QuantileRegressionSolver()
+    lambda_ = 1e6
+    with pytest.warns(UserWarning):    
+        quantreg.fit(x, y, tau, lambda_=lambda_, fit_intercept=True, save_problem=True)
 
 def test_regularization_without_intercept(random_data_no_weights):
     tau = 0.5
@@ -286,17 +297,8 @@ def test_ill_conditioned_warning():
     mu = np.asarray([1, 3, 5])
     sigma = np.asarray([[1, 0.9, 0], [0.9, 1, 0], [0, 0, 1]])
     x = random_number_generator.multivariate_normal(mu, sigma, size=3)
-    matrix_check = quantreg._check_matrix_condition(x)
-    assert not matrix_check
-
-    quantreg = QuantileRegressionSolver()
-
-    random_number_generator = np.random.RandomState(42)
-    mu = np.asarray([1, 3, 5])
-    sigma = np.asarray([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-    x = random_number_generator.multivariate_normal(mu, sigma, size=3)
-    matrix_check = quantreg._check_matrix_condition(x)
-    assert matrix_check
+    with pytest.warns(UserWarning):
+        matrix_check = quantreg._check_matrix_condition(x)
 
 
 ########################
@@ -312,20 +314,20 @@ def test_no_nan_inf_error(random_data_weights):
 
     x[0, 0] = np.nan
     with pytest.raises(ValueError):
-        quantreg.fit(x, y, tau)
+        quantreg.fit(x, y, tau, fit_intercept=False)
 
     x[0, 0] = np.inf
     with pytest.raises(ValueError):
-        quantreg.fit(x, y, tau)
+        quantreg.fit(x, y, tau, fit_intercept=False)
 
     x = random_data_weights[["x0", "x1", "x2", "x3", "x4"]].values
     y[5] = np.nan
     with pytest.raises(ValueError):
-        quantreg.fit(x, y, tau)
+        quantreg.fit(x, y, tau, fit_intercept=False)
 
     y[5] = np.inf
     with pytest.raises(ValueError):
-        quantreg.fit(x, y, tau)
+        quantreg.fit(x, y, tau, fit_intercept=False)
 
     quantreg.coefficients = [4, 32, 4, 24, 7]
     x = np.vstack([x, [4, 2, 6, np.nan, 3]])
