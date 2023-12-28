And apparently fixing a mistake I had made in the MAE formula...

tests/test_transition_solver.py
@@ -9,7 +9,7 @@ from elexsolver.TransitionSolver import TransitionSolver, mean_absolute_error
 def test_mean_absolute_error():
     Y = np.ones((5, 4))
     Y_pred = Y - 0.02
-    expected = 0.08
+    expected = 0.02
     current = np.around(mean_absolute_error(Y, Y_pred), 6)
     np.testing.assert_allclose(expected, current)
 