8e594c441c01583313840191f47288c0a30572e5
Diane Napolitano <diane.napolitano@washpost.com>
2024-01-30 14:14:05 -0500

    Adding a test for credible interval, increasing the number of samples, decreasing the tolerance

--- tests/test_ei_transition_solver.py
+++ tests/test_ei_transition_solver.py
@@ -4,8 +4,8 @@ import pytest
 from elexsolver.EITransitionSolver import EITransitionSolver
 
 # high tolerance to match PyMC's unit tests
-RTOL = 1e-01
-ATOL = 1e-01
+RTOL = 1e-02
+ATOL = 1e-02
 
 np.random.seed(1024)
 
@@ -33,9 +33,9 @@ def test_ei_fit_predict():
         ]
     )
 
-    expected = np.array([[0.34566509, 0.65433491], [0.40254308, 0.59745692]])
+    expected = np.array([[0.279297, 0.720703], [0.623953, 0.376047]])
 
-    ei = EITransitionSolver(random_seed=1024, n_samples=5, sampling_chains=1)
+    ei = EITransitionSolver(random_seed=1024, n_samples=10, sampling_chains=1)
     current = ei.fit_predict(X, Y)
     np.testing.assert_allclose(expected, current, rtol=RTOL, atol=ATOL)
 
@@ -66,9 +66,9 @@ def test_ei_fit_predict_with_weights():
 
     weights = np.array([500, 250, 125, 62.5, 31.25, 15.625])
 
-    expected = np.array([[0.34566509, 0.65433491], [0.40254308, 0.59745692]])
+    expected = np.array([[0.279297, 0.720703], [0.623953, 0.376047]])
 
-    ei = EITransitionSolver(random_seed=1024, n_samples=5, sampling_chains=1)
+    ei = EITransitionSolver(random_seed=1024, n_samples=10, sampling_chains=1)
     current = ei.fit_predict(X, Y, weights=weights)
     np.testing.assert_allclose(expected, current, rtol=RTOL, atol=ATOL)
 
@@ -96,9 +96,9 @@ def test_ei_fit_predict_pivoted():
         ]
     ).T
 
-    expected = np.array([[0.34566509, 0.65433491], [0.40254308, 0.59745692]])
+    expected = np.array([[0.279297, 0.720703], [0.623953, 0.376047]])
 
-    ei = EITransitionSolver(random_seed=1024, n_samples=5, sampling_chains=1)
+    ei = EITransitionSolver(random_seed=1024, n_samples=10, sampling_chains=1)
     current = ei.fit_predict(X, Y)
     np.testing.assert_allclose(expected, current, rtol=RTOL, atol=ATOL)
 
@@ -107,3 +107,36 @@ def test_ei_get_prediction_interval():
     with pytest.raises(NotImplementedError):
         ei = EITransitionSolver()
         ei.get_prediction_interval(0)
+
+
+def test_ei_credible_interval_percentages():
+    X = np.array(
+        [
+            [1, 2],
+            [3, 4],
+            [5, 6],
+            [7, 8],
+            [9, 10],
+            [11, 12],
+        ]
+    )
+
+    Y = np.array(
+        [
+            [2, 3],
+            [4, 5],
+            [6, 7],
+            [8, 9],
+            [10, 11],
+            [12, 13],
+        ]
+    )
+
+    expected_lower = np.array([[0.037212, 0.356174], [0.424652, 0.117605]])
+    expected_upper = np.array([[0.643826, 0.962788], [0.882395, 0.575348]])
+
+    ei = EITransitionSolver(random_seed=1024, n_samples=10, sampling_chains=1)
+    _ = ei.fit_predict(X, Y)
+    (current_lower, current_upper) = ei.get_credible_interval(99, transitions=False)
+    np.testing.assert_allclose(expected_lower, current_lower, rtol=RTOL, atol=ATOL)
+    np.testing.assert_allclose(expected_upper, current_upper, rtol=RTOL, atol=ATOL)
